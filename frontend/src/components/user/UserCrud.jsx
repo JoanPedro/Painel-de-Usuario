4765dc99aeb97947f1d6d05d1c7f4be8994bae46
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-11 16:42:09 -0300

    Componente UserCrud

---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Main from '../../template/Main'
+
+const headerProps = {
+  icon: "users",
+  title: "Usuários",
+  subtitle: "Cadastro de Usuários: C.R.U.D"
+}
+
+export default class UserCrud extends Component {
+  render() {
+    return (
+      <Main {...headerProps}>
+        Cadastro de Usuários
+      </Main>
+    )
+  }
+}
+
