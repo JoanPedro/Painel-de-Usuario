cace05ecbcca077d3b31528f0468406604713cc4
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-11 17:42:39 -0300

    Inicia Comunicao com Backend

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -7,7 +8,38 @@ const headerProps = {
   subtitle: 'Cadastro de Usuários: C.R.U.D'
 }
 
+const baseUrl = 'http://localhost:3004/users'
+const initialState = {
+  user: {name: '', email: ''},
+  list: []
+}
+
 export default class UserCrud extends Component {
+  state = {...initialState}
+
+  clear() {
+    this.setState({ user: initialState.user })
+  }
+
+  save() {
+    const user = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+
+    axios[method](url, user)
+      .then(res => {
+        const list = this.getUpdatedList(res.data)
+        this.setState({ user: initialState.user, list })
+      })
+  }
+
+  getUpdatedList(user) {
+    const list = this.state.list.filter(u => u.id !== user.id)
+    list.unshift(user)
+
+    return list
+  }
+
   render() {
     return (
       <Main {...headerProps}>
