b108d3313a671e13791863efb9672cdc2c95c860
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-11 22:27:58 -0300

    Finaliza

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# User-Controll
-Project in react for user controll
+<h1 align="center">
+    <img alt="Grid List w/ Filter" title="#Filter" src="" width="250px" />
+</h1>
+
+<h4 align="center">
+  🚀 Proof of Concept.
+</h4>
+<p align="center">
+  <img alt="Languages" src="https://img.shields.io/github/languages/count/JoanPedro/User-Controll">
+
+  <img alt="Size" src="https://img.shields.io/github/repo-size/JoanPedro/User-Controll">
+  
+  <a href="https://github.com/JoanPedro/User-Controll/commits/master">
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/JoanPedro/User-Controll">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-brightgreen">
+</p>
+
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-contribuir">Como contribuir</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+## :rocket: Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://pt-br.reactjs.org/)
+
+## 💻 Projeto
+
+O Projeto visa implementar um sistema de Controle para Usuários em React.
+
+![](.github/react-new.gif)
+## 🤔 Como contribuir
+
+- Faça um fork desse repositório;
+- Cria uma branch com a sua feature: `git checkout -b minha-feature`;
+- Faça commit das suas alterações: `git commit -m 'feat: Minha nova feature'`;
+- Faça push para a sua branch: `git push origin minha-feature`.
+
+Depois que o merge da sua pull request for feito, você pode deletar a sua branch.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -17,6 +17,12 @@ const initialState = {
 export default class UserCrud extends Component {
   state = {...initialState}
 
+  componentWillMount(){
+    axios(baseUrl).then(res => {
+      this.setState({ list: res.data })
+    })
+  }
+
   clear() {
     this.setState({ user: initialState.user })
   }
@@ -33,9 +39,9 @@ export default class UserCrud extends Component {
       })
   }
 
-  getUpdatedList(user) {
+  getUpdatedList(user, add = true) {
     const list = this.state.list.filter(u => u.id !== user.id)
-    list.unshift(user)
+    if(add) list.unshift(user)
 
     return list
   }
@@ -92,12 +98,64 @@ export default class UserCrud extends Component {
     )
   }
 
+  load(user){
+    this.setState({ user })
+  }
+
+  remove(user){
+    axios.delete(`${baseUrl}/${user.id}`).then(res => {
+      const list = this.getUpdatedList(user, false)
+      this.setState({ list })
+    })
+  }
+
+  renderTable(){
+    return(
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+    )
+  }
+
+  renderRows(){
+    return this.state.list.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button className="btn btn-warning"
+              onClick={ _ => this.load(user)}>
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button className="btn btn-danger ml-2"
+              onClick={ _ => this.remove(user)}>
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      )
+    })
+  }
+
   render() {
     return (
       <Main {...headerProps}>
         Cadastro de Usuários
         <hr />
         {this.renderForm()}
+        {this.renderTable()}
       </Main>
     )
   }
